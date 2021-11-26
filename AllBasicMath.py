4f6fcbdcdcfda7b5de51fdafd9ed2657baff2ed1
KRISHNA SINHA <94454194+PrinceKrishu31@users.noreply.github.com>
2021-11-26 23:53:19 +0530

    Add files via upload

---
+++ AllBasicMath.py
@@ -0,0 +1,10 @@
+a=float(input("enter first number="))
+b=float(input("enter second number="))
+sum=a+b
+print("sum=" + str(sum))
+sub=a-b
+print("substraction="+str(sub))
+mul=a*b
+print("multiplication of the two number="+str(mul))
+div=a/b
+print("division of first by second="+str(div))
